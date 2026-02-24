43e9d2bad86fb737645fa0b9999eddf2659636e9
vikramsaluja <vikram.saluja@menloschool.org>
2026-02-23 21:08:06 -0800

    Problem Set Completed.

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -16,8 +16,37 @@ public class PlagiarismChecker {
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
 
-        // TODO Complete this function to return the length of the longest shared substring.
+        // Lengths of both documents
+        int length1 = doc1.length();
+        int length2 = doc2.length();
 
-        return 0;
+        // Convert to character arrays for comparisons
+        char[] chars1 = doc1.toCharArray();
+        char[] chars2 = doc2.toCharArray();
+
+
+         // Table[i][j] represents the length of the longest shared sequence using
+         // The first i characters of doc1
+         // The first j characters of doc2
+        int[][] table = new int[length1 + 1][length2 + 1];
+
+        // Fill the table row by row
+        for (int i = 1; i <= length1; i++) {
+            for (int j = 1; j <= length2; j++) {
+
+                // If characters match, extend the sequence from the diagonal
+                if (chars1[i - 1] == chars2[j - 1]) {
+                    table[i][j] = table[i - 1][j - 1] + 1;
+                }
+                else {
+                    // Otherwise take the better result and ignore a character from doc1
+                    // Or ignore a character from doc2
+                    table[i][j] = Math.max(table[i - 1][j], table[i][j - 1]);
+                }
+            }
+        }
+
+        // The bottom right cell contains the final answer
+        return table[length1][length2];
     }
 }
